7ea550fc6a68a7eee7cf4d2845a372da9afdab4a
“PFerco” <ferco_1@hotmail.com>
2023-06-19 14:11:11 -0700

    Prints

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: Program that prints
+ *
+ * description: Program that prints
+ *
+ * return: zero on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
