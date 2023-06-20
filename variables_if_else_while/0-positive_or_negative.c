40a4bef3c017b2644fb300644855e5185fee1044
“PFerco” <ferco_1@hotmail.com>
2023-06-20 11:37:11 -0700

    Comparison

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that assigns random number
+ * Return: 0 All success
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+		printf("is positive");
+	else if (n = 0)
+		printf("is zero");
+	else printf("is negative");
+	return (0);
+}
