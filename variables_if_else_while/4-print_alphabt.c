8bab54eb5a5a4ecbbfff1318195d7dd74fdec8d7
“PFerco” <ferco_1@hotmail.com>
2023-06-20 12:59:57 -0700

    Printing alphabet without some

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program description.
+ * Return: 0 All success
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		putchar(character);
+	}
+	for (character = 'A'; character <= 'Z'; character++)
+	{
+		putchar(character);
+	}
+	putchar('\n');
+	return (0);
+}
