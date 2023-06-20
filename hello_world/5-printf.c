b0c01c82cb2f7fa3949497975ada9a149b29f918
“PFerco” <ferco_1@hotmail.com>
2023-06-20 08:38:57 -0700

    Using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that uses printf
+ * Return: 0 All succes
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
