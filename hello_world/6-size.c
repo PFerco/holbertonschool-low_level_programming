5283146ec7b5bda9f531f5f646128e7ed7024f5d
“PFerco” <ferco_1@hotmail.com>
2023-06-20 09:15:04 -0700

    Size of

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that prints the size of various types
+ * Return: 0 All success
+ */
+int main(void)
+{
+	printf ("Size of a char: %s", sizeof(char));
+	printf ("Size of an int: %s", sizeof(int));
+	printf ("Size of a long int: %s", sizeof(int long));
+	printf ("Size of a long long int: %s", sizeof(int long long));
+	printf ("Size of a float: %s", sizeof(float));
+}
