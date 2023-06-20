411093ce78ca3d7248e7bc33427ec8509e46344a
“PFerco” <ferco_1@hotmail.com>
2023-06-20 11:45:40 -0700

    Comparison

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,14 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n>0){
 		printf("is positive");
-	else if (n = 0)
+	}
+	else if (n = 0){
 		printf("is zero");
-	else printf("is negative");
+	}
+	else {
+		printf("is negative");
+	}
 	return (0);
 }
