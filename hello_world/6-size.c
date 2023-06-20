55700014e023c16078860504e13669180d863af2
“PFerco” <ferco_1@hotmail.com>
2023-06-20 09:25:58 -0700

    Size of

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: %s", sizeof(char));
-	printf("Size of an int: %s", sizeof(int));
-	printf("Size of a long int: %s", sizeof(int long));
-	printf("Size of a long long int: %s", sizeof(int long long));
-	printf("Size of a float: %s", sizeof(float));
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(int long));
+	printf("Size of a long long int: %lu\n", sizeof(int long long));
+	printf("Size of a float: %lu\n", sizeof(float));
 }
