017b58e5f2010e8b3f9576d3a0f096de5f9a9a7f
“PFerco” <ferco_1@hotmail.com>
2023-06-20 09:28:49 -0700

    Size of

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(int long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(int long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
