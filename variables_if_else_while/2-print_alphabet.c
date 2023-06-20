59fc7c857bf41000b36008d467db4ae4c2c4e4bc
“PFerco” <ferco_1@hotmail.com>
2023-06-20 12:43:06 -0700

    Printing alphabet in lower case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program description.
+ * Return: 0 All success
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 'A'; character <= 'Z'; character++)
+	{
+		character = tolower(character);
+		putchar(character);
+	}
+	return (0);
+}
