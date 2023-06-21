d4fc832796a7677452ba7a20846e3d399f5166e3
“PFerco” <ferco_1@hotmail.com>
2023-06-21 13:43:44 -0700

    Prints the alphabet, in lowercase.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point
  * Description: Program description.
  * Return: 0 All success
  */
-void main(void)
+void print_alphabet(void)
 {
 	char c;
 
@@ -13,6 +12,5 @@ void main(void)
 	{
 		_putchar(c);
 	}
-	_putchar('\n');
-	return (0);
+	_putchar('\n');i
 }
