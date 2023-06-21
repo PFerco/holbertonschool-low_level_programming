c67a9902e2e7eb87e19a9373c891bec10a4f3e86
“PFerco” <ferco_1@hotmail.com>
2023-06-21 16:42:42 -0700

    Function that computes the absolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point.
+ * @n: Variable
+ * Description: Program description.
+ * Return: n or -n All success
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
