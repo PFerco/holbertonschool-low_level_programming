fe1273c636506d6460a747ae11e331a4f4ef2ab7
“PFerco” <ferco_1@hotmail.com>
2023-06-21 00:28:13 -0700

    Prints all possible different combinations of three digits.

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
-				if (i != 8)
+				if (i != 7)
 				{
 					putchar(',');
 					putchar(' ');
