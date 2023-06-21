9dd75132cbd148a3e8b6ca8d0921fca5e79a02d6
“PFerco” <ferco_1@hotmail.com>
2023-06-21 00:50:59 -0700

    Prints all possible combinations of two two-digit numbers.

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main(void)
 		{
 			for (k = i; k < 10; k++)
 			{
-				for (l = j + 1; l < 10; l++)
+				for (l = j + 1; l <= 10; l++)
 				{
 					putchar((i % 10) + '0');
 					putchar((j % 10) + '0');
