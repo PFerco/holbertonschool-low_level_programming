9a9213af44f39bd17e19609a51c6819c4930f56f
“PFerco” <ferco_1@hotmail.com>
2023-06-21 00:00:13 -0700

    Prints all possible different combinations of two digits

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,17 @@
 int main(void)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 10; i++)
 	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+		}
 		putchar((i % 10) + '0');
-		if (i != 99)
+		if (i != 9 && j != 9)
 		{
 			putchar(',');
 			putchar(' ');
