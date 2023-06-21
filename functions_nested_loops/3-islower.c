481406bd98b6dd1ea4f175a2f8b1f67b789ded2f
“PFerco” <ferco_1@hotmail.com>
2023-06-21 14:03:54 -0700

    Function that checks for lowercase character.

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower() - Entry point
+ * Description: Program description.
+ * Return: 0 or 1 All success
+ */
+int _islower(int i)
+{
+	int i;
+
+	if (_islower(i) == 0)
+		return (0);
+	if (_islower(i) == 1)
+		return (1);
+}
