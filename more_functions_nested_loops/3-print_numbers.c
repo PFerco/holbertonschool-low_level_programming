7a5978bda35d0d2affeaebebfabee4ccc47cfcdb
“PFerco” <ferco_1@hotmail.com>
2023-06-22 12:55:47 -0700

    Function that prints the numbers, from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point.
+ * Description: Program description.
+ * Return: 0 All success
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar('i');
+		_putchar('\n');
+	}
+}
