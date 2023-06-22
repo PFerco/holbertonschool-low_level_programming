3603463fbb4b6475295a3db2d84b92df0ced57a7
“PFerco” <ferco_1@hotmail.com>
2023-06-22 12:33:41 -0700

    Function that checks for a digit (0 through 9).

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Entry point.
+ * @c: Variable
+ * Description: Program description.
+ * Return: 0 or 1 All success
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (1);
+}
