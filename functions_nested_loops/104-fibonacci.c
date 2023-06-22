e473144fc4e620ebb73d7a93e55730594d3cbade
“PFerco” <ferco_1@hotmail.com>
2023-06-21 22:35:12 -0700

    Program that finds and prints the first 98 Fibonacci numbers

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program description.
+ * Return: 0 All success
+ */
+int main(void)
+{
+	int i;
+	unsigned long n1 = 1, n2 = 2, sum;
+
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
+	for (i = 1; i <= 98; i++)
+	{
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+		printf("%lu", sum);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
