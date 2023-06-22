62076b1d80b2bbed706541cdc711ea2a70e6a3b8
“PFerco” <ferco_1@hotmail.com>
2023-06-21 21:08:18 -0700

    Function that prints the n times table, starting with 0.

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -17,10 +17,7 @@ void print_times_table(int n)
 			{
 				if ((i * j) / 10 > 0)
 				{
-					_putchar((i * j) / 100 + '0');
-				} else
-				{
-					_putchar(' ');
+					_putchar((i * j) / 100 + '0')
 				}
 				if ((i * j) / 10 > 0)
 				{
