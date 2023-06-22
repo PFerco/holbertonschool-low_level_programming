c552b0a4bc51cc66d58e8f42878c0b0c69d854c4
“PFerco” <ferco_1@hotmail.com>
2023-06-21 19:54:43 -0700

    Function that prints all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point.
+ * @n: Variable
+ * Description: Program description.
+ * Return: n or -n All success
+ */
+void print_to_98(int n)
+{
+	for ( ; n <= 98; n++)
+	{
+		_putchar(n);
+	}
+}
