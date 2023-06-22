68f27e95d4286ba74be767b182462c5c3abfd964
“PFerco” <ferco_1@hotmail.com>
2023-06-21 19:47:00 -0700

    Function that adds two integers and returns the result.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point.
+ * @n: Variable
+ * @m: Variable
+ * Description: Program description.
+ * Return: n or -n All success
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
