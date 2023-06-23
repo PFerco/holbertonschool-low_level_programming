fbb9899cc5f307aadd83acae5302cfe708a84786
“PFerco” <ferco_1@hotmail.com>
2023-06-23 09:11:30 -0700

    Function that draws a diagonal line on the terminal.

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point.
+ * @n: Variable
+ * Description: Program description.
+ * Return: Void All success
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
