d57d58c3b6da2c9a85d47d95c5b68243b5e762d0
“PFerco” <ferco_1@hotmail.com>
2023-06-23 08:57:23 -0700

    function that draws a straight line in the terminal.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - Entry point.
+ * @n: Variable
+ * Description: Program description.
+ * Return: Void All success
+ */
+void print_line(int n)
+{
+	int i;
+
+	if ( n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');	
+}
