c77fb7ea8b8335ebe25e6cd96f287551c70e1e10
“PFerco” <ferco_1@hotmail.com>
2023-06-25 18:09:43 -0700

    Program that finds and prints the largest prime factor

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	long n = 612852475143, np;
+	long n = 612852475143, np1, np2;
 
-	for (np = 2; (np < 612852475143 / 2) && (n % np == 0); )
+	for (np1 = 2; (np1 < 612852475143 / 2) && (n % np1 == 0); )
 	{
-		for ( ; n % np == 0; )
+		for (np2 = np1; n % np2 == 0; )
 		{
-			n /= np;
-			np++;
+			n /= np2;
+			np2++;
 		}
 	}
-	printf("%ld\n", np);
+	printf("%ld\n", np2);
 	return (0);
 }
